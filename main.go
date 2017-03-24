2a0f18abc6c381b14069828c7426538418a33590
wveik <kataev_06@list.ru>
2017-03-24 13:57:26 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	fmt.Println("Listening on port :3000")
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
+		fmt.Fprintf(w, "<h1>Hello World</h1>")
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
