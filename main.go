37917dd0d278a61738fcfa7d2c356fa1fc8581aa
wveik <kataev_06@list.ru>
2017-03-24 18:21:39 +0300

    Врисовал работу с Write.html

--- main.go
+++ main.go
@@ -15,11 +15,21 @@ func indexHandler (w http.ResponseWriter, r *http.Request)  {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func writeHandler(w http.ResponseWriter, r *http.Request)  {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil{
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+
 func main()  {
 	fmt.Println("Listening on port :3000")
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/write", writeHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
