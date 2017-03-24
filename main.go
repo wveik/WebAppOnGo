58055347065b25681a89069ad4aa3037bec96037
wveik <kataev_06@list.ru>
2017-03-24 14:11:16 +0300

    Вынес в отдельную функцию метод обработки входящий потоков

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
+func indexHandler (w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
 func main()  {
 	fmt.Println("Listening on port :3000")
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
-		fmt.Fprintf(w, "<h1>Hello World</h1>")
-	})
+	http.HandleFunc("/", indexHandler)
 
 	http.ListenAndServe(":3000", nil)
-	
+
 }
