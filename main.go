0a92d2cd386f66ac38e8b05e807a932d74798e98
wveik <kataev_06@list.ru>
2017-03-24 15:00:08 +0300

    Вывод CSS и шаблонов пользователю

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func indexHandler (w http.ResponseWriter, r *http.Request)  {
-	t, err := template.ParseFiles("templates/index.html")
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil{
 		fmt.Fprintf(w, err.Error())
 	}
@@ -18,6 +18,7 @@ func indexHandler (w http.ResponseWriter, r *http.Request)  {
 func main()  {
 	fmt.Println("Listening on port :3000")
 
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
 
 	http.ListenAndServe(":3000", nil)
