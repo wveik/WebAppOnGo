9881f1995591e34f67b35249420988918e9dc482
wveik <kataev_06@list.ru>
2017-03-27 16:01:04 +0300

    Добавлена сессия постов

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
+
+	"github.com/wveik/WebAppOnGo/models"
 )
+
+var posts map[string]*models.Post
 
 func indexHandler (w http.ResponseWriter, r *http.Request)  {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -25,15 +29,18 @@ func writeHandler(w http.ResponseWriter, r *http.Request)  {
 }
 
 func savePostHandler(w http.ResponseWriter, r *http.Request)  {
-	/*id := r.FormValue("id")
+	id := r.FormValue("id")
 	title := r.FormValue("title")
 	content := r.FormValue("content")
-*/
 
+	post := models.NewPost(id, title, content)
+	posts[post.Id]= post
 }
 
 func main()  {
 	fmt.Println("Listening on port :3000")
+
+	posts = make(map[string]*models.Post, 0)
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
